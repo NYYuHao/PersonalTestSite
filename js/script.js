498a9de99399d7877a2d226616d686abfac02d4b
Nyyuhao <Nyyuhao@gmail.com>
2023-04-04 23:39:54 -0400

    Adjusted animation timings

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const swup = new Swup();
 
 function navBarLinkClicked(element) {
-    console.log("Test");
     var navbar = document.getElementById("navbar");
     for (var child of navbar.children) {
         child.className = "";
